af20d372c0b5f3e0bc611fae448a0aff9a888dd8
UdithaIshan <uditha.ishan34@gmail.com>
2020-01-04 22:47:23 +0530

    Added flight class

--- 18000681.cpp
+++ 18000681.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
+
+class flight{
+    struct data{
+        int flight_no; 
+        string dateTime;
+        char dep_airport[20];
+        char arr_airport[20];
+        struct seat{
+            int row;
+            char seatClass;
+            char seatCode[6];
+        }seatArray[10];
+
+    public:
+        
+    };
+
+};
+
+int main(){
+    ofstream myfile("Flights.txt");
+
+}
